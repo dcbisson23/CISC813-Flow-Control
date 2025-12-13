0517a3fc53243ab6e233f4eea758c7f5307045eb
Donovan Bisson <19dcb2@queensu.ca>
2025-12-13 14:30:29 -0500

    Updated pretty much everything (really need to commit more often)

--- rddl_loader.py
+++ rddl_loader.py
@@ -3,69 +3,86 @@ import pyRDDLGym
 from pyRDDLGym.core.visualizer.movie import MovieGenerator
 
 from pyRDDLGym_jax.core.planner import JaxStraightLinePlan, JaxBackpropPlanner, JaxOfflineController, JaxDeepReactivePolicy, JaxOnlineController
+from pyRDDLGym_jax.core.tuning import JaxParameterTuning, Hyperparameter
 from pyRDDLGym_jax.core.planner import load_config
 from pyRDDLGym_jax.core.simulator import JaxRDDLSimulator
 from pathlib import Path
+
+import pandas as pd
+from IPython.display import Image
+from IPython.utils import io
+import matplotlib.pyplot as plt
+
 import os
 import csv
 import pprint
+import math
 
-# Two options for [Optimizer] planner
-# Copy-paste desired model into the [Optimizer] section, then run it
-"""
-method='JaxStraightLinePlan'
-method_kwargs={}
-
-method='JaxDeepReactivePolicy'
-method_kwargs={'topology': [128, 64]}
-"""
-
-CONFIG = """
+ONLINE_CONFIG = """
 [Model]
-logic='FuzzyLogic'
-logic_kwargs={}
-tnorm='ProductTNorm'
-tnorm_kwargs={}
-
+comparison_kwargs={'weight': 100}
+rounding_kwargs={'weight': 100}
+control_kwargs={'weight': 100}
 [Optimizer]
-method='JaxDeepReactivePolicy'
-method_kwargs={'topology': [128, 64]}
-optimizer='rmsprop'
-optimizer_kwargs={'learning_rate': 0.001}
-batch_size_train=32
-batch_size_test=32
+method='JaxStraightLinePlan'
+pgpe=None
 rollout_horizon=10
-
+utility='cvar'
+utility_kwargs={'alpha': 0.5}
 [Training]
 key=42
-epochs=5000
-train_seconds=30
+train_seconds=5
 """
 
-with open("config.cfg", "w") as f:
-    f.write(CONFIG)
+OFFLINE_CONFIG = """
+[Model]
+comparison_kwargs={'weight': 100}
+rounding_kwargs={'weight': 100}
+control_kwargs={'weight': 100}
+[Optimizer]
+method='JaxDeepReactivePolicy'
+optimizer_kwargs={'learning_rate': 0.001}
+utility='cvar'
+utility_kwargs={'alpha': 0.5}
+
+rollout_horizon=100
+[Training]
+key=42
+train_seconds=600
+"""
 
 
-domain = Path.cwd().joinpath('pump_domain.rddl')
-instance = Path.cwd().joinpath('pump_instance0.rddl')
+
+
+
+domain = Path.cwd().joinpath('POMDP/domain.rddl')
+instance = Path.cwd().joinpath('POMDP/instance0.rddl')
 
 # domain = Path.cwd().joinpath('archives/Reservoir/domain.rddl')
 # instance = Path.cwd().joinpath('archives/Reservoir/instance0.rddl')
 
-def run_planner(dom, prob, online=False):
 
+
+def run_planner(dom, prob, online=True):
+    with open("config.cfg", "w") as f:
+        if online:
+            f.write(ONLINE_CONFIG)
+        else:
+            f.write(OFFLINE_CONFIG)
     planner_args, plan_args, train_args = load_config("config.cfg")
     # os.system("rm -f logs/data_log.csv")
 
     # set up the environment (note the vectorized option must be True)
-    env = pyRDDLGym.make(domain=dom, instance=prob, vectorized=True, log_path=Path.cwd().joinpath("logs"))
+    env = pyRDDLGym.make(domain=dom, instance=prob, vectorized=True, backend=JaxRDDLSimulator, log_path=Path.cwd().joinpath("logs"))
+    print(env.action_space, env.state)
     recorder = MovieGenerator("logs", "pump-flow-ctrl", max_frames=env.horizon)
     env.set_visualizer(viz=None, movie_gen=recorder)
 
     if online:
         planner = JaxBackpropPlanner(rddl=env.model, **planner_args)
-        controller = JaxOnlineController(planner, train_seconds=2)
+        controller = JaxOnlineController(planner, **train_args)
         stats = controller.evaluate(env, episodes=1, verbose=False, render=True)
+        print(env.state)
 
     else:
         planner = JaxBackpropPlanner(rddl=env.model, **planner_args)
@@ -73,9 +90,29 @@ def run_planner(dom, prob, online=False):
         stats = controller.evaluate(env, episodes=1, verbose=False, render=True)
         pprint.pprint(stats)
 
+
+    # env.horizon = 30
+    
+    
+    # total_reward = 0
+    # state, _ = env.reset()
+    # print(f'Initial state = {state}')
+    # for step in range(10):
+    #     env.render()
+    #     action = controller.sample_action(state)
+    #     next_state, reward, terminated, truncated, _ = env.step(action)
+        
+    #     total_reward += reward
+    #     state = next_state
+    #     print(f'action = {action}, reward = {reward}, next state = {state}')
+    #     #print(terminated, truncated)
+    #     if terminated or truncated:
+    #         break
+
     rendered_image = env.render()
     env.close()
 
+    # print(total_reward)
     # remove the first 3 lines of the csv file
     # with open('logs/data_log.csv', 'r') as f:
     #     lines = f.readlines()
@@ -85,26 +122,5 @@ def run_planner(dom, prob, online=False):
 
     return rendered_image
 
-def show_actions():
-
-    actions = ['put-out', 'cut-out', 'water-drop']
-
-    with open('logs/data_log.csv', 'r') as f:
-        reader = csv.reader(f)
-
-        # get the columns from the header row
-        action_cols = []
-        header = next(reader)
-        for i, col in enumerate(header):
-            for action in actions:
-                if action in col:
-                    action_cols.append(i)
-
-        for row in reader:
-            print(f"{row[1]}:", end=" ")
-            for col in action_cols:
-                if row[col] == "True":
-                    print(f"{header[col]}", end=" ")
-            print()
-
-run_planner(domain, instance).show()
+if __name__ == '__main__':
+    run_planner(domain, instance).show()
